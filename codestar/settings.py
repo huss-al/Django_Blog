402d947b168c4a246cc7a7693f86bf184dc4d6d2
Huss Al Dafai <h_aldafai@hotmail.com>
2024-06-13 12:37:04 +0000

    changed debug to True

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--sf*+-ph%nk^ppiobm-2r62@@!71-zma+p+)mg1@($c)-4zn-3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost'
