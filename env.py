5e060823b55174548eb989224e291d74b09f48f3
Huss Al Dafai <h_aldafai@hotmail.com>
2024-06-20 12:37:51 +0000

    enable serving of image files

--- env.py
+++ env.py
@@ -4,3 +4,6 @@ os.environ.setdefault(
     "DATABASE_URL", "postgres://usrbewjyru9:cDHDsNL57JSw@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/cupid_curse_tree_127429")
 
 os.environ.setdefault("SECRET_KEY", "a1b2c3d4e5")
+
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://795635864859551:pMrLX3dcv3Ydvra3yqpX9Ar97ww@dqo3vn07c")
