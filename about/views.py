5e12145fc6849c74679353e786a4e49ce0573764
Huss Al Dafai <h_aldafai@hotmail.com>
2024-06-20 11:42:20 +0000

    wired up the collaboration form to handle POST request

--- about/views.py
+++ about/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from .models import AboutPage  # Import your model if you need to fetch data
+from django.contrib import messages
+from .models import AboutPage
 from .forms import CollaborateForm
 
 def about_view(request):
     """
     Renders the About page
     """
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
+
+
     about = AboutPage.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
 
